3e2030338b4b34923bd0684cacc7e3108e699c9d
Dr1xyDev <kilianmbappemessiento@gmail.com>
2026-06-10 17:30:56 -0500

    Refine comments in ItemTotem.java
    Updated comments for clarity and accuracy regarding the Totem of Undying's behavior and author.

--- src/main/java/cn/nukkit/item/ItemTotem.java
+++ src/main/java/cn/nukkit/item/ItemTotem.java
@@ -4,15 +4,17 @@ package cn.nukkit.item;
  * Totem of Undying
  *
  * Saves the player from death when held in offhand or main hand.
- * On activation:
- *   - Sets player health to 1 (instead of 0)
+ * Stack size is 1 (not stackable).
+ *
+ * Behavior (vanilla):
+ *   - Sets player health to 1 (instead of dying)
  *   - Gives Regeneration II (45 seconds / 900 ticks)
- *   - Gives Absorption II (20 seconds / 400 ticks)
+ *   - Gives Absorption II (5 seconds / 100 ticks)
  *   - Gives Fire Resistance I (40 seconds / 800 ticks)
- *   - Plays the totem animation on the client
+ *   - Plays the totem animation + sound
  *   - Consumes the totem item
  *
- * author: Dr1xyDev
+ * author: Dr1xy
  */
 public class ItemTotem extends Item {
 
@@ -26,5 +28,10 @@ public class ItemTotem extends Item {
 
     public ItemTotem(Integer meta, int count) {
         super(TOTEM, meta, count, "Totem of Undying");
+    }
+
+    @Override
+    public int getMaxStackSize() {
+        return 1;
     }
 }

828899b1c39e3255d6c11d78d28445be3a2ea936
Dr1xyDev <kilianmbappemessiento@gmail.com>
2026-06-10 17:28:20 -0500

    Add EVENT_SOUND_TOTEM constant to LevelEventPacket

--- src/main/java/cn/nukkit/network/protocol/LevelEventPacket.java
+++ src/main/java/cn/nukkit/network/protocol/LevelEventPacket.java
@@ -44,6 +44,7 @@ public class LevelEventPacket extends DataPacket {
     public static final int EVENT_SOUND_CAMERA_TAKE_PICTURE = 1050;
     public static final int EVENT_SOUND_EXPERIENCE_ORB = 1051;
     public static final int EVENT_SOUND_BLOCK_PLACE = 1052;
+    public static final int EVENT_SOUND_TOTEM = 1056;
 
     public static final int EVENT_GUARDIAN_CURSE = 2006;
     
